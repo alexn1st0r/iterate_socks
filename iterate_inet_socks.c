8a550a092750b084b94262e856912a304e42f6e0
anesterenko <anesterenko@cloudlinux.com>
2019-10-04 16:52:41 +0300

    init commit

---
+++ iterate_inet_socks.c
@@ -0,0 +1,116 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/list_nulls.h>
+#include <net/inet_hashtables.h>
+#include <net/sock.h>
+#include <net/tcp.h>
+
+struct inet_ctx {
+	struct inet_hashinfo *hashinfo;
+	bool (*filter)(struct sock *sk);
+	void (*cb)(struct sock *sk);
+};
+
+static int iterate_listening_socks(struct inet_ctx *ctx)
+{
+	struct inet_listen_hashbucket *ilb;
+	struct inet_hashinfo *hashinfo;
+	struct sock *sk;
+	unsigned int i;
+
+	if (!ctx ) {
+		printk(KERN_ERR "There is no ctx for iterate_established_tcp_socks\n");
+		return -EINVAL;
+	}
+
+	hashinfo = ctx->hashinfo;
+
+	/*
+	* NOTE(anesterenko)
+	* iterate all  buckets in tcp_hashinfo listensock table
+	*/
+	for (i = 0; i < INET_LHTABLE_SIZE; i++) {
+		ilb = &hashinfo->listening_hash[i];
+		sk = sk_head(&ilb->head);
+
+		sk_for_each_from(sk) {
+			if (ctx->filter(sk)) {
+				ctx->cb(sk);
+			}
+		}
+	}
+
+	return 0;
+}
+
+static int iterate_established_socks(struct inet_ctx *ctx)
+{
+	struct inet_ehash_bucket *ehash;
+	unsigned int ehash_mask;
+	struct sock *sk;
+	unsigned int i;
+
+	if (!ctx ) {
+		printk(KERN_ERR "There is no ctx for iterate_established_tcp_socks\n");
+		return -EINVAL;
+	}
+
+	ehash = ctx->hashinfo->ehash;
+	ehash_mask = ctx->hashinfo->ehash_mask;
+
+	/*
+	* NOTE(anesterenko)
+	* iterate all  buckets in tcp_hashinfo ehash table
+	*/
+	for (i = 0; i < ehash_mask; i++) {
+		struct hlist_nulls_node *node;
+		if (hlist_nulls_empty(&ehash[i].chain))
+			continue;
+
+		sk_nulls_for_each(sk, node, &ehash[i].chain) {
+			if (ctx->filter(sk)) {
+				ctx->cb(sk);
+			}
+		}
+	}
+
+	return 0;
+}
+
+static bool tcp_filter_wrapper(struct sock *sk)
+{
+	return true;
+}
+
+static void tcp_cb(struct sock *sk)
+{
+	printk(KERN_ERR "tcp sock [ %p ]\n", sk);
+}
+
+static int __init init_iterate(void)
+{
+	struct inet_ctx inet_walker[] = {
+		{
+			.hashinfo = &tcp_hashinfo,
+			.filter = tcp_filter_wrapper,
+			.cb = tcp_cb
+		}
+	};
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(inet_walker); i++) {
+		iterate_listening_socks(&inet_walker[i]);
+		iterate_established_socks(&inet_walker[i]);
+	}
+
+	return 0;
+}
+
+static void __exit exit_iterate(void)
+{
+	printk(KERN_ERR "Goodbye!\n");
+}
+
+module_init(init_iterate);
+module_exit(exit_iterate);
